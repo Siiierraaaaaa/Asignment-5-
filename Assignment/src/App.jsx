caf5915d5f61998554a27b9c06e847965c0c8768
Siiierraaaaaa <parkersierra40@gmail.com>
2026-03-14 13:59:48 -0400

    added search elements

--- Assignment/src/App.jsx
+++ Assignment/src/App.jsx
@@ -1,35 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [searchTerm, setSearchTerm] = useState('');
+
+
+
+  const handleSearch = (event) => {
+
+    // Update the searchTerm state here 
+    setSearchTerm(event.target.value)
+  };
+
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+    <div>
+
+      <h1>Search App</h1>
+
+      <Search searchTerm={searchTerm} onSearch={handleSearch} />
+
+      <p>Searching for: {searchTerm}</p>
+
+    </div>
+
+  );
+
 }
 
-export default App
+
+
+function Search(props) {
+
+  return (
+
+    <div>
+
+      <label htmlFor="search">Search: </label>
+
+      <input
+
+        id="search"
+
+        type="text"
+        value={props.searchTerm}
+        onChange={props.onSearch}
+      />
+        {/* Add value and onChange here */}
+
+
+
+    </div>
+
+  );
+
+}
+
+
+
+export default App; 
